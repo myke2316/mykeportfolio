afac53117b6f3d9b210a7f3359e22d47cf25854a
myke2316 <leianmykecastillo@gmail.com>
2025-01-11 10:53:25 +0800

    test1

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,23 @@
 import "./App.css";
 import AppLayout from "./components/AppLayout";
 import NavBar from "./components/NavBar";
-import { BrowserRouter, Route, Routes, useLocation } from "react-router-dom";
+import { HashRouter as Router, Route, Routes, useLocation } from "react-router-dom"; // Changed BrowserRouter to HashRouter
 import Home from "./pages/Home/Home";
 import About from "./pages/About/About";
 import Contact from "./pages/Contact/Contact";
 import Portfolio from "./pages/Portfolio/Portfolio";
 import Skills from "./pages/Skills/Skills";
 import { AnimatePresence } from "motion/react";
+
 const AnimatedRoutes = () => {
   const location = useLocation(); // Get the current location
-  console.log(location)
+  console.log(location);
+
   return (
     <AnimatePresence mode="wait">
       <Routes location={location} key={location.pathname}>
         <Route element={<AppLayout />}>
-          <Route element={<Home />} path="/" /> 
+          <Route element={<Home />} path="/" />
           <Route element={<About />} path="/about" />
           <Route element={<Skills />} path="/skills" />
           <Route element={<Portfolio />} path="/portfolio" />
@@ -27,9 +29,9 @@ const AnimatedRoutes = () => {
 };
 
 const App = () => (
-  <BrowserRouter basename="/mykeportfolio">
+  <Router>
     <AnimatedRoutes />
-  </BrowserRouter>
+  </Router>
 );
 
 export default App;
